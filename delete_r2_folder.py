b7a3880f9eb390d65e9a96388872916656aa5bf2
wwo115570-commits <wwo115570@gmail.com>
2026-08-10 04:26:00 +0300

    del

--- delete_r2_folder.py
+++ delete_r2_folder.py
@@ -11,7 +11,7 @@ CF_R2_ENDPOINT_URL = os.getenv('CF_R2_ENDPOINT_URL')
 BUCKET_NAME = os.getenv('CF_R2_BUCKET_NAME', '')
 
 
-FOLDER = os.getenv("FOLDER", "DKSA/year=2026/month=08/day=10")
+FOLDER = os.getenv("FOLDER", "DKSA/year=2026/month=08/day=8")
 if not FOLDER.endswith('/'):
     FOLDER += '/'
 

79dc45304995fce7e4f879dd6141fdf37e77973e
wwo115570-commits <wwo115570@gmail.com>
2026-08-11 08:55:10 +0300

    del

--- delete_r2_folder.py
+++ delete_r2_folder.py
@@ -10,7 +10,7 @@ CF_R2_SECRET_KEY = os.getenv("CF_R2_SECRET_ACCESS_KEY")
 CF_R2_ENDPOINT_URL = os.getenv("CF_R2_ENDPOINT_URL")
 BUCKET_NAME = os.getenv("CF_R2_BUCKET_NAME")
 
-FOLDER = "qatarsale"
+FOLDER = "DOMAN/monitor/"
 
 
 client = boto3.client(

e05ba3c1f063df3abdf28a0b2d5f8b7773e4b275
wwo115570-commits <wwo115570@gmail.com>
2026-08-18 15:15:32 +0300

    del Motors

--- delete_r2_folder.py
+++ delete_r2_folder.py
@@ -10,7 +10,7 @@ CF_R2_SECRET_KEY = os.getenv("CF_R2_SECRET_ACCESS_KEY")
 CF_R2_ENDPOINT_URL = os.getenv("CF_R2_ENDPOINT_URL")
 BUCKET_NAME = os.getenv("CF_R2_BUCKET_NAME")
 
-FOLDER = "DUAE/year=2026/month=08/day=16/motors/"
+FOLDER = "DUAE/year=2026/month=08/day=17/Motors/"
 
 
 client = boto3.client(
